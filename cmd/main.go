65453d9a361345a13c4479523440f00f4267aa8f
Habib Muhammad Rizki <habibboboho@gmail.com>
2025-10-01 15:10:16 +0700

    feat : add swagger in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,43 @@
-package cmd
+package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/habibmrizki/finalphase3/internals/configs"
+	"github.com/habibmrizki/finalphase3/internals/routers"
+	"github.com/joho/godotenv"
+)
+
+// @title 		Final Phase 3
+// @version		1.0
+// @description	RESTful API created using gin for Final Phase 3
+// @host		localhost:3000
+// @basepath	/
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Failed to load env\nCause", err.Error())
+		return
+	}
+	log.Println(os.Getenv("DBUSER"))
 
+	db, err := configs.InitDb()
+	if err != nil {
+		log.Println("Failed to connect to database\nCause", err.Error())
+		return
+	}
+	log.Println("DB CONNECTED")
+
+	defer db.Close()
+
+	rdb, err := configs.InitRedis()
+
+	if err != nil {
+		log.Println("Failed to connect to Redis\nCause", err.Error())
+		return
+	}
+	log.Println("Redis Connected")
+
+	router := routers.InitRouter(db, rdb)
+	router.Run(":3000")
 }
